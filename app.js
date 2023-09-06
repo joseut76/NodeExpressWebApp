44ed863d57aa37f2a6e11c153dbeae912e455735
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-05 22:05:10 -0600

    served static page index.html

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ import express from 'express';
 import chalk  from 'chalk'; 
 import debug from 'debug';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 //morgan middleware to use with debug
 app.use(morgan('tiny'))
+
+//__dirname && __filename are not available in ESModule
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//look in public directory forst for any request
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 }); 

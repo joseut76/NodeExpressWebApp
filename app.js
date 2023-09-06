463ac3c504144712cab9fdce47ef7689099601d9
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-06 14:51:06 -0600

    add template to page and remove unneded content

--- app.js
+++ app.js
@@ -25,7 +25,14 @@ app.set('views', './src/views')
 app.set('view engine', 'ejs') 
 
 sessionsRouter.route('/').get((req, res)=>{
-    res.render('sessions')
+    res.render('sessions', {
+        sessions:[
+            {title:'Session 1', description: 'this is session 1'},
+            {title:'Session 2', description: 'this is session 2'},
+            {title:'Session 3', description: 'this is session 3'},
+            {title:'Session 4', description: 'this is session 4'},
+        ]
+    })
 })
 
 sessionsRouter.route('/1').get((req, res)=>{

3650019947d4d926e2cb7a3438912e9bd206d3f4
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-05 19:50:09 -0600

    added debug package

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import chalk  from 'chalk'; 
+import debug from 'debug';
+
 
 const app = express();
 
-
-
 app.get('/', (req, res) => {
     res.send('Hello World')
-}) 
+}); 
 
-let port= 4000
+
+let port= 4000;
 app.listen(port, ()=>{
-    console.log('listening on port ' + chalk.green(port));
+    //runs only in dev mode
+    //use DEBUG=* node app.js to display debug mode
+    debug(`listening on port ${chalk.green(port)}`);
 } )

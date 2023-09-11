09fb3d25da3c267dbff00669313993e9e2240c9a
Jose Perdomo <jose.perdomo.dev@gmail.com>
2023-09-11 11:09:57 -0600

    add new user to a user collection in the mongo db

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,14 +1,37 @@
 import express from 'express';
-// import debug from "debug";
-// const debugAuth = debug('app:sessionsRouter')
+import debug from "debug";
+const debugAuth = debug('app:authRouter')
+import {MongoClient} from 'mongodb';
 
 const authRouter = express.Router();
 
 authRouter.route('/signUp').post((req, res)=>{
-    req.login(req.body, ()=>{
-        res.redirect('/auth/profile');
-    });
+
+    const {username, password} = req.body
+    const url = "mongodb+srv://joseut76:uoFeCMnSWlRU9k2Y@cluster0.ziadrhk.mongodb.net?retryWrites=true&w=majority";
+    const dbName = 'globomantics';
+
+    //self call function
+    (async function addUser(){
+        let client;
+        try {
+            client = await MongoClient.connect(url);
+
+            const db = client.db(dbName); 
+            const user = { username, password}
+            const results = await db.collection('users').insertOne(user);
+            debugAuth(results);
+            req.login(results.insertedId, ()=>{
+                res.redirect('/auth/profile')
+            });
+
+        } catch (error) {
+            debugAuth(error)
+        }
+        client.close()
+    })();
 });
+
 
 authRouter.route('/profile').get((req, res)=>{
     res.json(req.user);
